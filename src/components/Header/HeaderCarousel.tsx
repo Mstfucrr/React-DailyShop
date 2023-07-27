9a5f7b1d9cfd179375a7ec3398f13a7ec29a24e4
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-07-27 23:52:35 +0300

    add swiper

--- src/components/Header/HeaderCarousel.tsx
+++ src/components/Header/HeaderCarousel.tsx
@@ -1,7 +1,13 @@
-import React, { useState } from 'react'
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
 import {
-    MdArrowBackIosNew, MdArrowForwardIos
-} from 'react-icons/md';
+    SwiperSlide
+} from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 
 type Props = {
     images: string[]
@@ -9,61 +15,68 @@ type Props = {
 
 const HeaderCarousel = ({ images }: Props) => {
 
-    const [activeSlide, setActiveSlide] = useState(1);
 
-    const handlePrevSlide = (e : React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-        e.preventDefault();
-        setActiveSlide((prevSlide) => (prevSlide - 1 + images.length) % images.length);
-    };
+    const swiper = new Swiper(
+        '.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        autoplay: {
+            delay: 300,
+            disableOnInteraction: false,
+        },
+        speed: 700,
+        effect: 'fade',
+        fadeEffect: {
+            crossFade: true
+        },
 
-    const handleNextSlide = (e : React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-        e.preventDefault();
-        setActiveSlide((prevSlide) => (prevSlide + 1) % images.length);
-    };
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        modules: [Navigation, Pagination],
+    });
+
+
+
     return (
-        <div className="touch-pan-y relative">
-            <div className="relative w-full overflow-hidden">
-                <div className="relative w-full flex">
+        <div className="relative w-full overflow-hidden mt-1">
+            <div className="swiper-container h-[410px]"
+
+            >
+                <div className="swiper-wrapper">
                     {images.map((image, index) => (
-                        <div
-                            key={index}
-                            className={` flex-shrink-0 flex-grow-0 flex relative h-[410px] transition-all duration-500 ${index === activeSlide ? 'opacity-100 w-full' : 'opacity-0 w-0'
-                                }`}
-                        >
-                            <img src={image} alt="" className="w-full h-full object-cover" />
-                            <div className="absolute top-0 bottom-0 left-0 right-0 bg-black opacity-30"></div>
-                            <div className="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center">
-                                <div className="text-white text-center">
-                                    <h1 className="text-4xl font-semibold mb-4">New Collection</h1>
-                                    <p className="text-lg mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-                                    <a href="" className="inline-block bg-[#1C1C1C] text-center align-middle border border-solid border-transparent py-[.375rem] px-3
-                                    hover:bg-[#fff] hover:text-[#1C1C1C] transition-all duration-100
-                                    ">
-                                        Shop Now
-                                    </a>
-                                </div>
+                        <div key={index} className="swiper-slide">
+                            <img src={image} alt="" className="w-full h-full object-cover"
+                            />
+
+                            <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-30"></div>
+                            <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center">
+                                <h1 className="text-white text-4xl font-bold">New Collection</h1>
+                                <p className="text-white font-semibold
+                                    xl:text-2xl md:text-lg text-sm text-center
+                                ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                                <button className="px-8 py-3 bg-white text-black text-lg font-semibold mt-5 hover:bg-black hover:text-white hover:cursor-pointer hover:transition-all hover:duration-150">Shop Now</button>
                             </div>
+
                         </div>
                     ))}
                 </div>
-                <a href="" className="absolute top-0 bottom-0 flex items-center justify-center w-[15%] text-white text-center opacity-50 left-0
-                hover:opacity-90 transition-all duration-100"
-                    onClick={(e) => {
-                        handlePrevSlide(e);
-                    }}>
-                    <div className="inline-block bg-[#1C1C1C] text-center align-middle border border-solid border-transparent py-[.375rem] px-3">
-                        <MdArrowBackIosNew className="h-7 w-6" />
-                    </div>
-                </a>
-                <a href="" className="absolute top-0 bottom-0 flex items-center justify-center w-[15%] text-white text-center opacity-50 right-0
-                hover:opacity-90 transition-all duration-100"
-                    onClick={(e) => {
-                        handleNextSlide(e);
-                    }}>
-                    <div className="inline-block bg-[#1C1C1C] text-center align-middle border border-solid border-transparent py-[.375rem] px-3">
-                        <MdArrowForwardIos className="h-7 w-6" />
-                    </div>
-                </a>
+                <div className="swiper-button-prev h-full top-[5%] w-[13%]
+                        after:bg-black after:opacity-50 after:content-['prev'] after:text-white after:text-[30px] after:p-3
+                        after:hover:opacity-90 after:hover:cursor-pointer after:transition-all after:duration-150
+                    "></div>
+
+
+                <div className="swiper-button-next h-full top-[5%] w-[13%]
+                        after:bg-black after:opacity-50 after:content-['next'] after:text-white after:text-[30px] after:p-3
+                        after:hover:opacity-90 after:hover:cursor-pointer after:transition-all after:duration-150
+                    "></div>
 
             </div>
         </div>
