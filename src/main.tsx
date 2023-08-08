704dae2d999c34be3ebd149375b073ae92978602
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-08-08 13:46:39 +0300

    add primereact and its CSS

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+    
+//core
+import "primereact/resources/primereact.min.css";    
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
