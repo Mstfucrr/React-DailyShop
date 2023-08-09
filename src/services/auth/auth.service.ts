5574992a2a07af7c754fc5d8c116dade84e4e117
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-08-09 03:45:37 +0300

    add auth service

---
+++ src/services/auth/auth.service.ts
@@ -0,0 +1,45 @@
+import axios from "axios";
+import {
+    ILogin , IRegister
+} from "./types";
+
+const login = async (input: ILogin) => {
+    // const { data } = await axios.post('https://api.dailyshop.com/api/Auth/sign-in', { input });
+    // for testing
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTE"
+    console.log("input", input)
+    const data = {
+        "status": 200,
+        "message": "Login successfully",
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+        "data": {
+            "id": 1,
+            "email": input.email,
+            "firstName": "John",
+            "lastName": "Doe",
+            "role": "admin",
+            "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMTE"
+        }
+    }
+    return [null, data]
+
+    // for error testing
+    // const data = {
+    //     "status": 400,
+    //     "message": "Login failed",
+    //     'Content-Type': 'application/json',
+    //     "data": null
+    // }
+    // return [data, null]
+}
+
+const register = async (input: IRegister) => {
+    const { data } = await axios.post('https://api.dailyshop.com/api/Auth/sign-up', { input });
+    return data;
+}
+
+export const authService = {
+    login,
+    register
+}
---
+++ src/services/auth/types.ts
@@ -0,0 +1,12 @@
+export interface ILogin {
+    email: string;
+    password: string;
+}
+
+export interface IRegister {
+    termAndConditionId: number;
+    name: string;
+    surname: string;
+    email: string;
+    password: string;
+}
