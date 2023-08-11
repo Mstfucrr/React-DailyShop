8e49718f7dfef77cbe50188e55f47af1d7dcbfa5
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-08-11 20:29:59 +0300

    fix page style

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -12,8 +12,8 @@ const index = (props: Props) => {
         <>
             <Navbar />
 
-            <div className='md:w-[500px] w-full md:h-[520px] h-auto md:my-0 pb-10 bg-white 
-            border border-solid mx-auto md:mt-[150px] md:pb-0 '>
+            <div className='md:w-[500px] w-full h-auto md:my-0 pb-10 bg-white 
+            border border-solid mx-auto md:mt-[150px] '>
                 <div className='py-7 bg-primary flex justify-center items-center mb-4'>
                     <h1 className='text-white md:text-4xl text-3xl font-bold text-center'>Login</h1>
                 </div>
