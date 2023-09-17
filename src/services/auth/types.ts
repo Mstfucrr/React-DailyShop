995ca14ba89916d3fe2e48872999fcb19432c7e0
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-09-17 14:53:48 +0300

    add IUser and IUserAddress types

--- src/services/auth/types.ts
+++ src/services/auth/types.ts
@@ -10,3 +10,28 @@ export interface IRegister {
     email: string;
     password: string;
 }
+
+export interface IUser {
+    id: number;
+    name: string;
+    surname: string;
+    email: string;
+    role: string;
+    createdAt: string;
+    updatedAt: string;
+    profileImage: string;
+    phone: string;
+    addresses: IUserAddress[];
+}
+
+export interface IUserAddress {
+    id: number;
+    title : string;
+    description : string;
+    isMain : boolean;
+    address: string;
+    city: string;
+    state: string;
+    country: string;
+    zipCode: string;
+}
