031e89302fd72fb692a921bf135bd9660bb4edb7
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-09-26 15:44:40 +0300

    add example user data

--- src/components/account/account.tsx
+++ src/components/account/account.tsx
@@ -1,6 +1,7 @@
 import { IUser, IUserAddress } from "@/services/auth/types"
 import { AnimatePresence, motion } from "framer-motion"
 import { useEffect, useState } from "react"
+import { user } from "./example.user"
 import UserInformation from "./userInformation"
 import UserOrders from "./userOrders"
 
@@ -13,40 +14,7 @@ enum AccountTabs {
 
 const Account = () => {
     const [activeTab, setActiveTab] = useState(AccountTabs.USER_INFO)
-    const user: IUser = {
-        id: 1,
-        name: 'Mehmet',
-        surname: 'Kaya',
-        email: 'mhmtky123@gmail.com',
-        phone: '532 123 45 67',
-        profileImage: 'https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?w=826&t=st=1694862140~exp=1694862740~hmac=3367954bc36e90d64d76202dced34ed4e2f575ca512ef8a182b2e0a6fa185212',
-        addresses : [
-            {
-                id: 1,
-                title: 'Ev',
-                address: 'İstanbul / Kadıköy',
-                description: 'Ev adresi',
-                isMain: true,
-                city: 'İstanbul',
-                country: 'Turkey',
-                zipCode : 12124
-            },
-            {
-                id : 2,
-                title: 'İş',
-                description: 'İş adresi',
-                isMain: false,
-                city: 'Ankara',
-                address: 'Ankara / Çankaya / Kızılay',
-                country: 'Turkey',
-                zipCode : 14124
-            }
-        ] as IUserAddress[],
-        role : "User",
-        createdAt : "2021-09-01T00:00:00.000Z",
-        updatedAt : "2023-09-01T00:00:00.000Z",
-        
-    }
+    
 
     const [userState, setUserState] = useState<IUser>(user)
 
---
+++ src/components/account/example.user.ts
@@ -0,0 +1,36 @@
+import { IUser, IUserAddress } from "@/services/auth/types";
+
+export const user: IUser = {
+    id: 1,
+    name: 'Mehmet',
+    surname: 'Kaya',
+    email: 'mhmtky123@gmail.com',
+    phone: '532 123 45 67',
+    profileImage: 'https://img.freepik.com/free-psd/3d-illustration-person-with-sunglasses_23-2149436188.jpg?w=826&t=st=1694862140~exp=1694862740~hmac=3367954bc36e90d64d76202dced34ed4e2f575ca512ef8a182b2e0a6fa185212',
+    addresses : [
+        {
+            id: 1,
+            title: 'Ev',
+            address: 'İstanbul / Kadıköy',
+            description: 'Ev adresi',
+            isMain: true,
+            city: 'İstanbul',
+            country: 'Turkey',
+            zipCode : 12124
+        },
+        {
+            id : 2,
+            title: 'İş',
+            description: 'İş adresi',
+            isMain: false,
+            city: 'Ankara',
+            address: 'Ankara / Çankaya / Kızılay',
+            country: 'Turkey',
+            zipCode : 14124
+        }
+    ] as IUserAddress[],
+    role : "User",
+    createdAt : "2021-09-01T00:00:00.000Z",
+    updatedAt : "2023-09-01T00:00:00.000Z",
+    
+}
