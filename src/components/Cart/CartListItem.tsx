8d6c809d913b8094ea62e5afc58e9515e7ee929b
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-09-26 15:44:53 +0300

    fix cart components

--- src/components/Cart/CartListItem.tsx
+++ src/components/Cart/CartListItem.tsx
@@ -1,58 +1,71 @@
 import { useEffect, useState } from 'react'
 import { FaMinus, FaPlus, FaTimes } from 'react-icons/fa'
-import { Product } from './CartList'
+import { ICartItem } from '@/shared/types'
 
 
 
-const CartListItem = (product: Product) => {
-  const [quantity, setQuantity] = useState(product.quantity)
-  const [total, setTotal] = useState(product.total)
+
+const CartListItem = (
+  { cartItem, setCartItems }: { cartItem: ICartItem, setCartItems: React.Dispatch<React.SetStateAction<ICartItem[]>> }
+) => {
+  const [quantity, setQuantity] = useState(cartItem.quantity)
+  const [total, setTotal] = useState(0)
 
 
   useEffect(() => {
-    setTotal((quantity * parseInt(product.price.replace('$', ''))).toString())
+    setTotal((quantity * cartItem.product.price))
+    setCartItems((prev) => {
+      const index = prev.findIndex((item) => item.id === cartItem.id)
+      const newCartItems = [...prev]
+      newCartItems[index].quantity = quantity
+      return newCartItems
+    })
+    console.log("q")
   }, [quantity])
+
+  const handleRemoveItem = (id: number) => {
+    setCartItems((prev) => prev.filter((item) => item.id !== id))
+  }
 
   return (
     <tr className='bg-white'>
       <td className='p-3 border border-solid border-secondary align-middle'>
-        <img src={product.image} width={50} alt="" 
-        className='inline-block object-cover w-12 h-12 rounded-md'
+        <img src={cartItem.product.image} width={50} alt=""
+          className='inline-block object-cover w-12 h-12 rounded-md'
         />
-        <span className='ml-3'>{product.name}</span>
+        <span className='ml-3'>{cartItem.product.name}</span>
       </td>
       <td className='p-3 border border-solid border-secondary align-middle'>
-        {product.price}
+        {cartItem.product.price}
       </td>
       <td className='p-3 border border-solid border-secondary align-middle'>
         <div className="relative flex flex-nowrap justify-center items-stretch mx-auto max-w-[100px]">
-          
+
           <div>
             <button className='inline-block bg-primary text-[#212529] border-primary py-2 px-2 leading-6 
                   hover:text-white hover:bg-primaryDark transition-all
                   duration-300 ease-in-out'
-                  onClick={() => setQuantity(quantity - 1)}
-                  disabled={quantity === 1}
+              onClick={() => setQuantity(quantity - 1)}
+              disabled={quantity === 1}
 
-                  >
+            >
               <FaMinus className='' />
-            </button> 
+            </button>
           </div>
-           
+
           <input type="text" className='relative w-[1%] flex-[1_1_auto] text-center bg-secondary py-1 px-2
                 outline-none text-sm'
-                // just number
-                value={quantity}
-                onChange={(e) => setQuantity(parseInt(e.target.value))}
-                min={1}
-                />
+            value={quantity}
+            onChange={(e) => setQuantity(parseInt(e.target.value))}
+            min={1}
+          />
           <div>
             <button className='inline-block bg-primary text-[#212529] border-primary py-2 px-2 leading-6 
                   hover:text-white hover:bg-primaryDark transition-all
                   duration-300 ease-in-out'
-                  onClick={() => setQuantity(quantity + 1)}
+              onClick={() => setQuantity(quantity + 1)}
 
-                  >
+            >
               <FaPlus className='' />
             </button>
           </div>
@@ -65,7 +78,9 @@ const CartListItem = (product: Product) => {
       <td className='p-3 border border-solid border-secondary align-middle'>
         <button className='inline-block bg-primary text-[#212529] border-primary py-2 px-2 leading-6
               hover:text-white hover:bg-primaryDark transition-all
-              duration-300 ease-in-out'>
+              duration-300 ease-in-out'
+          onClick={() => { handleRemoveItem(cartItem.id) }}
+        >
           <FaTimes className='' />
         </button>
 
---
+++ src/components/Cart/index.tsx
@@ -0,0 +1,122 @@
+import { useEffect, useState } from 'react'
+import { products } from '../shop/example.products'
+import CartListItem from './CartListItem'
+import { ICartItem } from '@/shared/types'
+const Cart = () => {
+
+
+
+    const cartItemsExample: ICartItem[] = [
+        {
+            id: 1,
+            product: products[0],
+            quantity: 1,
+        },
+        {
+            id: 2,
+            product: products[2],
+            quantity: 2,
+        }, {
+            id: 3,
+            product: products[0],
+            quantity: 1,
+        },
+        {
+            id: 4,
+            product: products[2],
+            quantity: 2,
+        }
+
+    ]
+
+    const [cartItems, setCartItems] = useState<ICartItem[]>(cartItemsExample)
+    const [cartTotal, setCartTotal] = useState(
+        cartItems.reduce((total, cartItem) => total + (cartItem.product.price * cartItem.quantity), 0)
+    )
+
+    useEffect(() => {
+        setCartTotal(cartItems.reduce((total, cartItem) => total + (cartItem.product.price * cartItem.quantity), 0))
+    }, [cartItems])
+
+    return (
+        <div className="grid lg:grid-rows-1 grid-rows-2 grid-flow-col lg:px-16 gap-9 my-24 px-[15px]">
+            <div className="grid lg:col-span-12 col-span-12">
+                <table className="w-100 text-[#6F6F6F] mb-0 text-center border-0 border-collapse">
+                    <thead className="bg-secondary text-black">
+                        <tr>
+                            <th className='p-3 border border-solid border-secondary'>Ürünler</th>
+                            <th className='p-3 border border-solid border-secondary'>Fiyat</th>
+                            <th className='p-3 border border-solid border-secondary'>Miktar</th>
+                            <th className='p-3 border border-solid border-secondary'>Toplam</th>
+                            <th className='p-3 border border-solid border-secondary'>Kaldır</th>
+                        </tr>
+                    </thead>
+                    <tbody className='align-middle'>
+                        {
+                            cartItems.map((cartItem) => (
+                                <CartListItem key={cartItem.id} cartItem={cartItem} setCartItems={setCartItems} />
+                            ))
+                        }
+                    </tbody>
+
+                </table>
+            </div>
+            <div className="grid lg:col-span-4 col-span-12">
+                <form action="" className="mb-12">
+                    <div className="relative flex flex-wrap w-full items-stretch">
+                        <input className="relative flex-auto p-6 border border-solid outline-none border-secondary
+                                    text-[#212529] placeholder-[#212529] placeholder-opacity-50
+                                    h-[50px] "
+                            type="text" placeholder="Coupon code" />
+                        <div className="-ml-[1px] flex">
+                            <button className="bg-primary border border-solid border-transparent text-[#212529] py-[0.375rem] px-3">
+                                Apply Coupon
+                            </button>
+                        </div>
+                    </div>
+                </form>
+                {/* card */}
+                <div className="mb-12 border border-solid border-secondary
+                        flex flex-col relative h-min">
+                    {/* card header */}
+                    <div className="py-3 px-5 bg-secondary">
+                        <h4 className="font-semibold text-2xl text-black">
+                            Sepet Özeti
+                        </h4>
+                    </div>
+                    {/* card body */}
+                    <div className="flex-auto p-5">
+                        <div className="flex justify-between mb-4 pt-1">
+                            <h6 className="font-medium text-black">
+                                Ara Toplam
+                            </h6>
+                            <h6 className="font-medium text-black">
+                                ${cartTotal}
+                            </h6>
+                        </div>
+                    </div>
+                    {/* card footer */}
+                    <div className="border border-solid border-secondary py-3 px-5">
+                        <div className="flex justify-between mt-2 text-black">
+                            <h5 className="font-bold text-xl">
+                                Toplam
+                            </h5>
+                            <h5 className="font-bold text-xl">
+                                ${cartTotal}
+                            </h5>
+                        </div>
+                        <button className="bg-primary border border-solid border-transparent text-[#212529] py-4 px-3 mt-4 w-full
+                                hover:bg-primaryDark hover:border-primaryDark hover:text-white
+                                transition duration-300 ease-in-out
+                            ">
+                            Ödeme İşlemine Geçin
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Cart
