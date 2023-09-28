b83ea9e7830b2393b2831b70c0974dabebe92fa7
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-09-28 14:41:21 +0300

    add cartItemsExample

--- src/components/shop/example.products.ts
+++ src/components/shop/example.products.ts
@@ -19,8 +19,8 @@
 */}
 
 
-import { IProduct, IShopResponse } from "../../shared/types";
-import { user } from "../account/example.user";
+import { ICartItem, IProduct, IShopResponse } from "../../shared/types";
+import { reviews } from "../account/example.review";
 
 import p1 from "./img/product-1.jpg";
 import p2 from "./img/product-2.jpg";
@@ -58,29 +58,7 @@ export const products = [
             status : "Yeni",
             stock : 17
         },
-        reviewsData : [
-            {
-                id : 1,
-                date : new Date("2021-06-01T00:00:00"),
-                productId : 1,
-                rating : 3,
-                user : user,
-                review : "Diam amet duo labore stet elitr ea clita ipsum, tempor labore accusam ipsum et no at. Kasd diam tempor rebum magna dolores sed sed eirmod ipsum.",
-                name : user.name,
-                avatar : "https://bootdey.com/img/Content/avatar/avatar1.png",
-                email : user.email
-            },
-            {
-                id : 2,
-                date : new Date("2021-06-01T00:00:00"),
-                productId : 1,
-                rating : 4,
-                name : "John Doe",
-                email : "jhondoe@example.com",
-                review : "Diam amet duo labore stet elitr ea clita ipsum, tempor labore accusam ipsum et no at. Kasd diam tempor rebum magna dolores sed sed eirmod ipsum.",
-                avatar : "https://bootdey.com/img/Content/avatar/avatar2.png"
-            }
-        ]
+        reviewsData : reviews
     },
     {
         id: 2,
@@ -337,3 +315,25 @@ export const getProducts = async (
     })
 }
 
+export const cartItemsExample: ICartItem[] = [
+    {
+        id: 1,
+        product: products[0],
+        quantity: 1,
+    },
+    {
+        id: 2,
+        product: products[2],
+        quantity: 2,
+    }, {
+        id: 3,
+        product: products[0],
+        quantity: 1,
+    },
+    {
+        id: 4,
+        product: products[2],
+        quantity: 2,
+    }
+
+]
