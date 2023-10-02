31f85b32bc0a38995b6b09ca34413a0db6400a3c
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-10-02 04:33:04 +0300

    add formik

--- src/components/Seller/ProductInfo.tsx
+++ src/components/Seller/ProductInfo.tsx
@@ -2,12 +2,14 @@ import { Dropdown, DropdownChangeEvent } from 'primereact/dropdown';
 import { TreeSelect, TreeSelectChangeEvent } from 'primereact/treeselect';
 import { InputNumber } from 'primereact/inputnumber';
 import { InputText } from 'primereact/inputtext';
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { categories, Category, convertCategoriesToTreeSelectModel, findCategoryByKeyInTreeSelectModel } from '../shop/example.products';
 import { TreeNode } from "primereact/treenode";
 import { MultiSelect } from 'primereact/multiselect';
 import { Editor, EditorTextChangeEvent } from "primereact/editor";
 import { Button } from 'primereact/button';
+import * as Yup from 'yup'
+import { useFormik } from 'formik'
 
 type Props = {}
 
@@ -44,6 +46,80 @@ const ProductInfo = (props: Props) => {
     const [stock, setStock] = useState<number>(0)
     const [description, setDescription] = useState<string>('')
 
+    const validationSchema = Yup.object().shape({
+        productName: Yup.string()
+            .required('Ürün adı gereklidir')
+            .min(3, 'Ürün adı çok kısa')
+            .max(50, 'Ürün adı çok uzun'),
+        price: Yup.number()
+            .required('Fiyat gereklidir')
+            .min(2, 'Fiyat 2 dan küçük olamaz'),
+        stock: Yup.number()
+            .required('Stok gereklidir')
+            .min(0, 'Stok 0 dan küçük olamaz'),
+        description: Yup.string()
+            .required('Açıklama gereklidir')
+            .min(40, 'Açıklama çok kısa')
+            .max(500, 'Açıklama çok uzun'),
+        status: Yup.string()
+            .required('Durum gereklidir'),
+
+    })
+
+    const formik = useFormik({
+        initialValues: {
+            productName: '',
+            price: 0,
+            stock: 0,
+            description: '',
+            status: '',
+            category: '',
+        },
+        validationSchema: validationSchema,
+        validate: (data) => {
+            let errors = {}
+            if (!data.category) {
+                errors = { ...errors, category: 'Kategori seçiniz' }
+            }
+            return errors
+        },
+        onSubmit: (values) => {
+            console.log("values: ", values);
+            // ex. conn service and send data
+            /*
+                const data = {
+                    productName: values.productName,
+                    price: values.price,
+                    stock: values.stock,
+                    description: values.description,
+                    status: values.status,
+                    category: selectedCategory.id,
+                    colors: selectedColors,
+                    sizes: selectedSizes,
+                    
+                }
+
+                const productService = () => {
+                    addProduct: async (data: any) => {
+                        const response = await axios.post('/api/products/add', data)
+                        return response
+                    }
+                }
+
+                const ps = await productService.addProduct(data)
+                if (ps.status === 200) {
+                    // success
+                } else {
+                    // error
+                }
+            
+
+
+             */
+        }
+    })
+
+
 
 
     const colorTemplete = (option: any) => {
@@ -69,37 +145,66 @@ const ProductInfo = (props: Props) => {
         }
     }, [selectedCategory, selectedNodeKey]);
 
+    const isFormFieldInvalid = (name: string) => !!((formik.touched[name] && formik.errors[name]));
+
+    const getFormErrorMessage = (name: string) => {
+        return isFormFieldInvalid(name) ? <small className="p-error block h-0 mb-6">{formik.errors[name]}</small> : null;
+    };
+
+
     return (
         <>
             <div className="flex h-auto flex-col gap-y-7">
                 <h2 className="text-4xl font-semibold mb-5">Ürün Bilgileri</h2>
-                <form className="flex flex-col gap-y-4" >
+                <form className="flex flex-col gap-y-4"
+                    onSubmit={formik.handleSubmit}
+                >
 
                     {/* category */}
                     <span className="p-float-label">
                         <TreeSelect id='ts-category' name='ts-category'
-                            className='w-full md:w-56'
-                            value={selectedNodeKey} options={TreeNodes}
-                            onChange={(e: TreeSelectChangeEvent) => setSelectedNodeKey(e.value as any)}
+                            className={`w-full md:w-56 ${formik.touched.category && formik.errors.category ? 'p-invalid' : ''}`}
+                            value={formik.values.category}
+                            options={TreeNodes}
+                            onChange={(e: TreeSelectChangeEvent) => {
+                                formik.setFieldValue('category', e.value);
+                                console.log(e);
+                                setSelectedNodeKey(e.value)
+                            }}
                             filter
+
 
                         />
                         <label htmlFor="ts-category">
                             Bir Kategori Seç
                         </label>
+                        {getFormErrorMessage('category')}
+
                     </span>
+
 
                     {/* product name and pierce */}
                     <div className="flex flex-row flex-wrap gap-5 w-full">
 
                         <div className="w-full md:w-1/2">
                             <label htmlFor="in-product-name" className="font-bold block mb-2">Ürün Adı</label>
-                            <InputText id="in-product-name" name="in-product-name" className="w-full " />
+                            <InputText id="in-product-name" name="in-product-name"
+                                className={`w-full ${formik.touched.productName && formik.errors.productName ? 'p-invalid' : ''}`}
+                                value={formik.values.productName}
+                                onChange={(e) => formik.setFieldValue('productName', e.target.value)}
+                            />
+
+                            {getFormErrorMessage('productName')}
                         </div>
                         <div className="w-full md:w-1/2 flex gap-x-3 flex-col">
                             <label htmlFor="in-product-price" className="font-bold block mb-2">Fiyatı</label>
-                            <InputNumber id="in-product-price" name="in-product-price" className="w-full mb-3 "
-                                mode="currency" currency="TRY" locale="de-DE" minFractionDigits={2} />
+                            <InputNumber id="in-product-price" name="in-product-price"
+                                mode="currency" currency="TRY" locale="de-DE" minFractionDigits={2}
+                                className={`w-full mb-2 ${formik.touched.price && formik.errors.price ? 'p-invalid' : ''}`}
+                                onChange={(e) => formik.setFieldValue('price', e.value as any)}
+                                value={formik.values.price}
+                            />
+                            {getFormErrorMessage('price')}
 
                             {/* fiyat önerisi getir */}
                             <div className="flex items-center gap-x-2 flex-row flex-wrap">
@@ -159,7 +264,7 @@ const ProductInfo = (props: Props) => {
                     <div className="flex flex-row flex-wrap gap-x-5 w-full">
                         <div className="w-full md:w-1/3">
                             <label htmlFor="dd-status" className="font-bold block mb-2">Durum</label>
-                            <Dropdown id="dd-status" name="dd-status" className="w-full "
+                            <Dropdown id="dd-status" name="dd-status"
                                 options={[
                                     { label: 'Yeni', value: 'new' },
                                     { label: 'İkinci El', value: 'second-hand' },
@@ -167,15 +272,24 @@ const ProductInfo = (props: Props) => {
                                     { label: 'Kullanılmış', value: 'used' },
                                     { label: 'Kötü', value: 'bad' },
                                 ]}
-                                value={selectedStatus}
-                                onChange={(e: DropdownChangeEvent) => setSelectedStatus(e.value as any)}
+                                onChange={(e: DropdownChangeEvent) => {
+                                    formik.setFieldValue('status', e.value as any);
+                                }}
+                                value={formik.values.status}
+                                className={`w-full ${formik.touched.status && formik.errors.status ? 'p-invalid' : ''}`}
                             />
+
+                            {getFormErrorMessage('status')}
+
                         </div>
 
                         <div className="w-full md:w-1/3">
                             <label htmlFor="in-stock" className="font-bold block mb-2">Stok Adedi</label>
-                            <InputNumber id="in-stock" name="in-stock" className="w-full " value={stock}
-                                onValueChange={(e) => setStock(e.value as any)} />
+                            <InputNumber id="in-stock" name="in-stock" min={1}
+                                onChange={(e) => formik.setFieldValue('stock', e.value as any)}
+                                className={`w-full ${formik.touched.stock && formik.errors.stock ? 'p-invalid' : ''}`}
+                                value={formik.values.stock}
+                            />
                         </div>
                     </div>
 
@@ -183,21 +297,22 @@ const ProductInfo = (props: Props) => {
                     <div className="flex w-full mt-5 ">
                         <div className="w-full card mx-auto">
                             <label htmlFor="ed-description" className="font-bold block mb-2">Açıklama</label>
-                            <Editor value={description} onTextChange={(e: EditorTextChangeEvent) => setDescription(e.htmlValue as any)}
+                            <Editor value={formik.values.description}
+                                onTextChange={(e: EditorTextChangeEvent) => formik.setFieldValue('description', e.htmlValue as any)}
                                 style={{ height: '300px' }}
                                 id="ed-description" name="ed-description"
-
+                                className={`w-full ${formik.touched.description && formik.errors.description ? 'border-red-500 border' : ''}`}
                             />
+                            {getFormErrorMessage('description')}
+
                         </div>
                     </div>
 
                     {/* SUBMİT */}
 
-                    <Button className='!bg-primary !border-primary text-white mt-5
+                    <Button className='!bg-primary !border-primary text-white !mt-7
                         hover:!bg-primaryDark w-1/2
                     ' label='Kaydet' type='submit' />
-
-
                 </form>
 
             </div>
