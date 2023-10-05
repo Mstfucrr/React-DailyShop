b2ede6cef4e49854c41f22adad457de4ff8e9bea
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-10-05 18:55:14 +0300

    add admin access

---
+++ src/services/admin/admin.service.ts
@@ -0,0 +1,19 @@
+import { getTokenFromCookie } from "@/helper/cookieUtils"
+
+
+
+export const getAdminAccess = async (token: string) => {
+    // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlMzAyNzVkMTQxNDAxNjc5ZTM4MzJhMSIsIm5hbWUiOiJBZG1pbiIsImlhdCI6MTY5NjQ1Mzc5MywiZXhwIjoxNjk2NDU0MzkzfQ.TbuiK5KTbtZ_guULX4BwZ8tQuW-Mjo7x1re1WUvFClk"
+    
+    
+    const response = await fetch('http://localhost:5000/api/admin', {
+        headers : {
+            'Accept': '*/*',
+            'Authorization' : `Bearer: ${token}`
+        }
+    })
+    const data = await response.json()
+    console.log("data : " , data)
+    return data
+
+}
