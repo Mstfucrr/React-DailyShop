6a119a16a4a9c170c558872daa081edbdd07cada
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-10-05 18:53:48 +0300

    add persistReducer

--- src/main.tsx
+++ src/main.tsx
@@ -11,11 +11,17 @@ import 'primeicons/primeicons.css';
 import "primereact/resources/primereact.min.css";
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistor = persistStore(store)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <PersistGate persistor={persistor}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
   </Provider>
 )
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,32 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
-import { toastSlice } from './Toast';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+import toast from './Toast'
+import auth from './auth'
+import thunk, { ThunkDispatch } from 'redux-thunk';
 
-export const store = configureStore({
-    reducer: {
-        toast: toastSlice.reducer,
-    },
+export const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const reducers = combineReducers({
+    toast,
+    auth,
 });
 
-export type RootState = ReturnType<typeof store.getState>;
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const middlewares = [thunk]
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware : middlewares,
+});
+
+export type RootState = ReturnType<typeof reducers>;
 export type AppDispatch = typeof store.dispatch;
+export type AppThunk = ThunkDispatch<RootState, unknown, any>;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 
