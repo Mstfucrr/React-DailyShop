d68c09f813d0ace34da630c703bc99bdc874e91c
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-10-22 23:50:18 +0300

    add navigate

--- src/components/auth/loginForm.tsx
+++ src/components/auth/loginForm.tsx
@@ -65,6 +65,7 @@ const LoginForm = () => {
                     dispatch(SET_ADMIN_AUTH())
                     navigate("/admin")
                 }
+                navigate("/")
             }
         },
     })
