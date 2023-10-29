66ba221fa56a66de06ff2b53d4b731bdee7a1752
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-10-30 00:12:28 +0300

    add ISiteSettings type

---
+++ src/services/admin/types.ts
@@ -0,0 +1,8 @@
+export interface ISiteSettings {
+    id: number | undefined;
+    about: string | undefined;
+    address: string | undefined;
+    siteIcon: File | undefined;
+    email: string | undefined;
+    phone: string | undefined;
+}
