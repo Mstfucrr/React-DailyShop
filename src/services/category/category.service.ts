3be20042db4d54bab83734610cd3bd79fc2c20b6
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-11-02 19:45:42 +0300

    add category service

---
+++ src/services/category/category.service.ts
@@ -0,0 +1,8 @@
+import { makeRequest } from "../base/base";
+
+const fetchCategories = async () =>
+    await makeRequest<any>("Categories/GetList", "GET");
+
+export default {
+    fetchCategories
+}
