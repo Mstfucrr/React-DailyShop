da318e28066eac51ca94fbccd713c236bc4b0bcc
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-11-06 16:37:27 +0300

    fix base serivce

--- src/services/base/base.ts
+++ src/services/base/base.ts
@@ -1,33 +1,32 @@
-import axios, { AxiosResponse, Method, AxiosError } from 'axios';
+import axios, { AxiosResponse, Method } from 'axios';
 
 const apiBaseUrl = "http://localhost:5025/api";
 
 export const makeRequest = async <T>(url: string, method: Method, data?: any, token?: string): Promise<T> => {
-    return new Promise<T>(async (resolve, reject) => {
-        const headers: Record<string, string> = {
-            "Content-Type": "application/json",
-        };
-        if (token) {
-            headers.Authorization = `Bearer ${token}`;
-        }
-        const api = axios.create({
-            baseURL: apiBaseUrl,
-            headers,
-        });
+    const headers: Record<string, string> = {
+        "Content-Type": "application/json",
+    };
+    if (token) {
+        headers.Authorization = `Bearer ${token}`;
+    }
 
-        try {
-            const response: AxiosResponse<T> = await api.request({
-                url,
-                method,
-                data,
-            });
-            resolve(response.data);
-        } catch (error : any) {
-            if (axios.isAxiosError(error)) {
-                reject(new Error(error.response?.data?.Message || error.message));
-            } else {
-                reject(new Error(error));
-            }
-        }
+    const api = axios.create({
+        baseURL: apiBaseUrl,
+        headers,
     });
-}
+
+    try {
+        const response: AxiosResponse<T> = await api.request({
+            url,
+            method,
+            data,
+        });
+        return response.data; // Return the data directly
+    } catch (error: any) {
+        if (axios.isAxiosError(error)) {
+            throw new Error(error.response?.data?.Message || error.message); // Throw an error
+        } else {
+            throw new Error(error); // Throw an error
+        }
+    }
+};
