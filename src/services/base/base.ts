19ac8f34479a690d1eaa23b14d761a0dc8580490
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-11-09 04:43:33 +0300

    update base service for "multipart/form-data"

--- src/services/base/base.ts
+++ src/services/base/base.ts
@@ -2,13 +2,18 @@ import axios, { AxiosResponse, Method } from 'axios';
 
 const apiBaseUrl = "http://localhost:5025/api";
 
-export const makeRequest = async <T>(url: string, method: Method, data?: any, token?: string): Promise<T> => {
+export const makeRequest = async <T>(url: string, method: Method, data?: any, token?: string, hasFile: boolean = false): Promise<T> => {
+
+    // eğer datanın içinde file varsa multipart/form-data olarak gönderiyoruz yoksa application/json olarak gönderiyoruz
     const headers: Record<string, string> = {
-        "Content-Type": "application/json",
-    };
-    if (token) {
-        headers.Authorization = `Bearer ${token}`;
+        "Content-Type": hasFile
+            ? `multipart/form-data; boundary=${generateBoundary()}`
+            : "application/json",
     }
+
+    if (token)
+        headers.Authorization = `Bearer ${token}`;
+
 
     const api = axios.create({
         baseURL: apiBaseUrl,
@@ -23,10 +28,19 @@ export const makeRequest = async <T>(url: string, method: Method, data?: any, to
         });
         return response.data; // Return the data directly
     } catch (error: any) {
-        if (axios.isAxiosError(error)) {
-            throw new Error(error.response?.data?.Message || error.message); // Throw an error
-        } else {
-            throw new Error(error); // Throw an error
-        }
+        console.log(error.response?.data?.Message || error.message)
+        throw new Error(axios.isAxiosError(error) ? (error.response?.data?.Message || error.message) : error); // Throw an error
     }
 };
+
+function generateBoundary() {
+    let boundary = "--------------------------"; // Örnek bir başlangıç değeri
+
+    for (let i = 0; i < 24; i++) {
+        // 24 karakterlik rastgele bir dize oluşturun
+        const randomChar = Math.floor(Math.random() * 36).toString(36);
+        boundary += randomChar;
+    }
+
+    return boundary;
+}
