310cec61ce94e3478f5cd7211c7ea7904ac4ce57
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-11-18 19:12:07 +0300

    update addToCart

--- src/services/order/order.service.ts
+++ src/services/order/order.service.ts
@@ -2,10 +2,10 @@ import { IaddToCartRequest } from "./types";
 import { makeRequest } from "../base/base";
 
 export const getCart = async (token: string) => 
-    await makeRequest<any>("Cart", "GET", null, token);
+    await makeRequest<any>("Carts", "GET", null, token);
 
-export const addToCart = async (input: IaddToCartRequest, token: string) => 
-    await makeRequest<any>("Cart", "POST", input, token);
+export const addToCart = async (productId: number,input: IaddToCartRequest, token: string) => 
+    await makeRequest<any>(`Carts?productId=${productId}`, "POST", input, token);
 
 export const removeFromCart = async (id: number, token: string) => 
     await makeRequest<any>(`Cart/${id}`, "DELETE", null, token);
