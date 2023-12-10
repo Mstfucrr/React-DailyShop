d240bf5f4a9e00511350a50b454ae16589276573
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-12-10 17:26:18 +0300

    fix setUpdateProductId

--- src/components/account/ProductCard.tsx
+++ src/components/account/ProductCard.tsx
@@ -3,10 +3,11 @@ import { Button } from "primereact/button"
 import { Card } from "primereact/card"
 
 type Props = {
-    product: IProduct
+    product: IProduct,
+    setUpdateProductId: (updateProductId: number | null) => void
 }
 
-const ProductCard = ({ product }: Props) => {
+const ProductCard = ({ product, setUpdateProductId }: Props) => {
 
     return (
         <Card
@@ -14,7 +15,9 @@ const ProductCard = ({ product }: Props) => {
                 <div className="flex flex-row justify-between gap-7 flex-wrap">
                     <h3 className="text-2xl">{product.name}</h3>
                     <div className="flex flex-row gap-4">
-                        <Button label="Düzenle" rounded severity="warning" icon="pi pi-pencil" />
+                        <Button label="Düzenle" rounded severity="warning" icon="pi pi-pencil"
+                            onClick={() => setUpdateProductId(product?.id)} 
+                        />
                         <Button label="Sil" rounded severity="danger" icon="pi pi-trash" />
                     </div>
                 </div>
