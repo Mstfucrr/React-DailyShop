c13fe8b88def62be1fe0a3d18e262ea46ed3132f
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-12-12 02:26:24 +0300

    fix fetchSettings service url

--- src/services/admin/settings.service.ts
+++ src/services/admin/settings.service.ts
@@ -2,7 +2,7 @@ import { makeRequest } from "../base/base";
 import { ISiteSettings } from "./types";
 
 export const fetchSettings = async (token: string) =>
-    await makeRequest<any>(`Admin/SiteSettings`, "GET", null, token);
+    await makeRequest<any>(`Admin/WebSiteSettings`, "GET", null, token);
 
 export const saveSettings = async (val: ISiteSettings, token: string) =>
-    await makeRequest<any>(`Admin/SiteSettings`, "PUT", val, token , true);
+    await makeRequest<any>(`Admin/WebSiteSettings`, "PUT", val, token , true);
