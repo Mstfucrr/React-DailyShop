15c39de76d01740ff29f6849db71e115e2bde092
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-12-12 19:49:23 +0300

    add IOrder and IOrderItem types

--- src/services/order/types.ts
+++ src/services/order/types.ts
@@ -1,6 +1,36 @@
+import { IProduct } from "@/shared/types";
+import { IUser } from "../auth/types";
 
 export interface IaddToCartRequest {
     quantity: number;
     size: string | undefined;
     color: string | undefined;
+}
+
+export interface IOrder {
+    id: number | undefined;
+    address: IOrderAddress;
+    status : boolean | undefined;
+    user: IUser;
+    totalPrice : number;
+    orderItems : IOrderItem[];
+    date: string;
+}
+
+export interface IOrderItem {
+    id: number | undefined;
+    product : IProduct;
+    price : number;
+    quantity : number;
+    size : string;
+    color: string;
+}
+
+export interface IOrderAddress {
+    id: number;
+    title : string;
+    address: string;
+    city: string;
+    state: string;
+    country: string;
 }
