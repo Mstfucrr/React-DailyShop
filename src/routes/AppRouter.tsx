1c81366bec20a2e7674e1681b481a615f8a3bbf1
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-12-12 17:29:46 +0300

    add react react-helmet package and use in AppRouter

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import Home from '@/pages/home';
 import Shop from '@/pages/shop';
@@ -14,10 +14,12 @@ import { useDispatch, useSelector } from 'react-redux';
 import { authSelector } from '@/store/auth';
 import { SET_TOAST } from '@/store/Toast';
 import { IToast } from '@/store/Toast/type';
+import AboutUsPage from '@/pages/AboutUs';
+import { Helmet } from 'react-helmet';
 
 const PrivateRoute: React.FC<{ path: string, element: React.ReactNode }> = ({ path, element }) => {
     const dispatch = useDispatch();
-    const { isAuthorized , auth } = useSelector(authSelector);
+    const { isAuthorized, auth } = useSelector(authSelector);
     useEffect(() => {
         if (isAuthorized && auth.id) return;
         localStorage.setItem('from', path);
@@ -32,18 +34,46 @@ const PrivateRoute: React.FC<{ path: string, element: React.ReactNode }> = ({ pa
 };
 
 const AppRouter: React.FC = () => {
+    const TopLevelHelmet = useCallback((title: string) => {
+        return <Helmet>
+            <title>{title}</title>
+        </Helmet>
+    }, [])
+    
     return (
         <Routes>
-            <Route path="/" Component={Home} />
-            <Route path="/shop/:id" Component={Shop} />
-            <Route path="/product/:id" Component={ProductDetailPage} /> {/* Updated path */}
-            <Route path="/login" Component={Login} />
-            <Route path="/register" Component={Register} />
-            <Route path="/cart" element={<PrivateRoute path="/cart" element={<Cart />} />} />
-            <Route path="/seller" element={<PrivateRoute path="/seller" element={<SellerPage />} />} />
-            <Route path="/checkout" Component={CheckoutPage} />
-            <Route path="/account/:tab?" element={<PrivateRoute path="/account" element={<Account />} />} />
-            <Route path="/admin/*" element={<PrivateRoute path="/admin" element={<Admin />} />} />
+            <Route path="/"
+                element={<>{TopLevelHelmet("Anasayfa")}<Home /></>} />
+                
+            <Route path="/shop/:id"
+                element={<>{TopLevelHelmet("Mağaza")}<Shop /></>} />
+                
+            <Route path="/product/:id"
+                element={<>{TopLevelHelmet("Ürün Detayı")}<ProductDetailPage /></>} />
+                
+            <Route path="/login"
+                element={<>{TopLevelHelmet("Giriş Yap")}<Login /></>} />
+                
+            <Route path="/register"
+                element={<>{TopLevelHelmet("Kayıt Ol")}<Register /></>} />
+                
+            <Route path="/about"
+                element={<>{TopLevelHelmet("Hakkımızda")}<AboutUsPage /></>} />
+                
+            <Route path="/cart" element={
+                <>{TopLevelHelmet("Sepetim")}<PrivateRoute path="/cart" element={<Cart />} /></>} />
+
+            <Route path="/seller" element={
+                <>{TopLevelHelmet("Satıcı Paneli")}<PrivateRoute path="/seller" element={<SellerPage />} /></>} />
+
+            <Route path="/checkout" element={
+                <>{TopLevelHelmet("Ödeme Yap")}<PrivateRoute path="/checkout" element={<CheckoutPage />} /></>} />
+
+            <Route path="/account/:tab?" element={
+                <>{TopLevelHelmet("Hesabım")}<PrivateRoute path="/account" element={<Account />} /></>} />
+
+            <Route path="/admin/*" element={
+                <>{TopLevelHelmet("Admin Paneli")}<PrivateRoute path="/admin" element={<Admin />} /></>} />
         </Routes>
     );
 };
