bc73c5b39c30af432987cff5b75bf9c38ce26a22
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-12-12 17:30:42 +0300

    add fetchAndUpdateSiteIcon with service

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import to from "await-to-js";
 import { Toast } from "primereact/toast";
 import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter } from "react-router-dom"
 import Footer from "./components/Footer";
 import AppRouter from "./routes/AppRouter"
+import { getSiteIcon } from "./services/siteIcon/siteIcon";
 import { CLEAR_TOAST } from "./store/Toast";
 import { IToast } from "./store/Toast/type";
 
@@ -29,9 +31,24 @@ function App() {
       });
 
 
-    dispatch(CLEAR_TOAST())
+      dispatch(CLEAR_TOAST())
     }
   }, [toastData])
+
+  // site Icon 
+  const fetchAndUpdateSiteIcon = async () => {
+    const link = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
+    const [err, data] = await to(getSiteIcon())
+    if (err) return console.log(err)
+    if (data) {
+      link.href = data.data
+    }
+  }
+
+  useEffect(() => {
+    fetchAndUpdateSiteIcon()
+  }, [])
+
 
   return (
     <BrowserRouter>
---
+++ src/services/siteIcon/siteIcon.ts
@@ -0,0 +1,4 @@
+import { makeRequest } from "../base/base";
+
+export const getSiteIcon = async () =>
+    await makeRequest<any>(`/Abouts/GetIcon`, "GET");
