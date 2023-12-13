cdd6d14124661a92e4c30cfa010e4cb19a800bc9
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-12-13 17:53:48 +0300

    Create a new types.ts file under the shop service and add the ProductsSortBy enum to this file

---
+++ src/services/shop/types.ts
@@ -0,0 +1,11 @@
+
+export enum ProductsSortBy {
+    Newest = 'newest',
+    PriceLowToHigh = 'priceLowToHigh',
+    PriceHighToLow = 'priceHighToLow',
+    NameAZ = 'nameAZ',
+    NameZA = 'nameZA',
+    TopRated = 'topRated',
+    Review = 'review'
+}
+
