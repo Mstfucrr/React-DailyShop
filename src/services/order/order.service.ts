14f0f0659a9aa5030ef331d0b612d913c6d006c8
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-12-17 02:32:39 +0300

    Add order-related functions to order.service.ts

--- src/services/order/order.service.ts
+++ src/services/order/order.service.ts
@@ -1,6 +1,7 @@
-import { IaddToCartRequest } from "./types";
+import { IOrderRequest, IaddToCartRequest } from "./types";
 import { makeRequest } from "../base/base";
 
+// Cart
 export const getCart = async (token: string) => 
     await makeRequest<any>("Carts", "GET", null, token);
 
@@ -12,3 +13,13 @@ export const updateCart = async (id: number, input: any, token: string) =>
 
 export const removeFromCart = async (id: number, token: string) => 
     await makeRequest<any>(`Carts/Delete/${id}`, "DELETE", null, token);
+
+// Order
+export const getOrders = async (token: string) => 
+    await makeRequest<any>("Orders", "GET", null, token);
+
+export const getOrder = async (id: number, token: string) =>
+    await makeRequest<any>(`Orders/${id}`, "GET", null, token);
+
+export const createOrder = async (input: IOrderRequest, token: string) =>
+    await makeRequest<any>("Orders", "POST", input, token);
