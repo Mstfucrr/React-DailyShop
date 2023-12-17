2215b95f27ee72f40681ddab186b74bf74938a9b
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-12-18 00:24:18 +0300

    Add ForgotPassword page

---
+++ src/pages/ForgotPassword/index.tsx
@@ -0,0 +1,21 @@
+import Navbar from "@/components/Navbar"
+import Searchbar from "@/components/Searchbar"
+import Topbar from "@/components/Topbar"
+import ForgotPasswordForm from "@/components/auth/forgotPasswordForm"
+
+
+const ForgotPassword = () => {
+
+    return (
+        <>
+
+            <Topbar />
+            <Searchbar />
+            <Navbar />
+            <ForgotPasswordForm />
+        </>
+    )
+
+}
+
+export default ForgotPassword
