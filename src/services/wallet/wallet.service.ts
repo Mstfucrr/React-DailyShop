86ffdd91e885baa63bf50c70b314d5a727d8e563
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-12-18 05:18:27 +0300

    Add wallet service with getWalletByUser and addMoneyToWallet functions

---
+++ src/services/wallet/wallet.service.ts
@@ -0,0 +1,9 @@
+import { makeRequest } from "../base/base";
+
+export const getWalletByUser = async (token : string) =>
+    await makeRequest<any>(`Wallets/GetWalletByUser`, "GET", null, token);
+
+export const addMoneyToWallet = async (input: any, token: string) =>
+    await makeRequest<any>(`Wallets/AddMoneyToWallet`, "POST", input, token);
+
+
