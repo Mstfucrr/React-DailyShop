4454a3d6f7989706c0aea324384601dea0384b04
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-12-18 19:19:10 +0300

    Refactor UserOrders component to fetch and display user orders

--- src/components/account/userOrders/userOrders.tsx
+++ src/components/account/userOrders/userOrders.tsx
@@ -8,52 +8,56 @@ import { DataView } from 'primereact/dataview';
 import { Fieldset } from "primereact/fieldset"
 import { Steps } from "primereact/steps"
 import { Rating } from "primereact/rating"
+import { getOrders } from "@/services/order/order.service"
+import to from "await-to-js"
+import { IOrder, IOrderAddress, IOrderItem } from "@/services/order/types"
+import { Link } from "react-router-dom"
 
 const UserOrders = () => {
 
-  const [orders, setOrders] = useState([])
+  const [orders, setOrders] = useState<IOrder[]>([])
   const { token } = useSelector(authSelector)
   const steps = orderStatus.map((item) => ({ label: item.label }))
 
   const msgs = useRef<Messages>(null)
 
   const fetchOrders = async () => {
-    // const [err, data] = await to(getOrders(token))
-    // if (err) {
-    //   msgs.current?.clear()
-    //   msgs.current?.show([
-    //     { sticky: true, severity: 'error', summary: 'Sistematik Hata', detail: err.message , closable: false}
-    //   ]);
-    //   return
-    // }
-    // if (data.data) {
-    //   setOrders(data.data)
-    // }
+    const [err, data] = await to(getOrders(token))
+    if (err) {
+      msgs.current?.clear()
+      msgs.current?.show([
+        { sticky: true, severity: 'error', summary: 'Sistematik Hata', detail: err.message, closable: false }
+      ]);
+      return
+    }
+    if (data.data) {
+      setOrders(data.data)
+    }
   }
 
   useEffect(() => {
     fetchOrders()
   }, [])
 
-  const legendTemplate = () => {
+  const legendTemplate = (order: IOrder) => {
     return <div className="flex md:flex-row flex-col sm:items-center !text-sm justify-between text-white">
-      <span className="ml-1">Sipariş No: <span className="text-primary">123123</span></span>
-      <span className="ml-1">Tarih: <span className="text-primary">12.12.2021</span></span>
-      <span className="ml-1">Toplam: <span className="text-primary">12.12 TL</span></span>
-      <span className="ml-1">Durum: <span className="text-primary">Hazırlanıyor</span></span>
+      <span className="ml-1">Sipariş No: <span className="text-primary">{order.OrderNumber}</span></span>
+      <span className="ml-1">Tarih: <span className="text-primary">{order.date}</span></span>
+      <span className="ml-1">Toplam: <span className="text-primary">{order.totalPrice} ₺</span></span>
+      <span className="ml-1">Durum: <span className="text-primary">{orderStatus.find((item) => item.value === order.status)?.label}</span></span>
     </div>
 
   }
 
-  const orderAddressTemplate = () => {
+  const orderAddressTemplate = (address: IOrderAddress) => {
     return (
       <div className="w-full flex flex-col gap-2">
         <h3 className="text-2xl text-primaryDark font-semibold">Teslimat Adresi</h3>
         <div className="w-full flex flex-col gap-2">
-          <span className="">Adres Adı</span>
-          <span className="">Adres</span>
-          <span className="">Şehir</span>
-          <span className="">Posta Kodu</span>
+          <span className="font-semibold">{address.title}</span>
+          <span className="">{address.address}</span>
+          <span className="">{address.city}</span>
+          <span className="">{address.zipCode}</span>
 
         </div>
 
@@ -61,48 +65,24 @@ const UserOrders = () => {
 
     )
   }
-  const exCartItems = [{
-    product: {
-      image: 'https://picsum.photos/200/300',
-      name: 'Jacket',
-      price: 48.00,
-      rating: 3,
-    },
-    id: 1,
-    price: 48.00,
-    color: 'red',
-    size: 'M',
-    quantity: 1
-  }, {
-    product: {
-      image: 'https://picsum.photos/200/300',
-      name: 'Jacket',
-      price: 48.00,
-      rating: 3,
-    },
-    id: 1,
-    price: 48.00,
-    color: 'red',
-    size: 'M',
-    quantity: 1
-  }]
 
-
-  const OrderItemTemplate = (CartItem: any) => {
+  const OrderItemTemplate = (orderItem: IOrderItem) => {
     return (
       <div className="flex m-2 shadow-lg px-5 py-8 w-full">
-        <div className="flex flex-row flex-wrap gap-5 justify-between">
-          <div className="sm:w-1/2 w-full">
-            <img src={CartItem.product.image} alt={CartItem.product.name} className="w-full h-auto object-scale-down" />
-          </div>
+        <div className="flex flex-row flex-wrap gap-5 items-center">
+          <Link className="sm:w-1/3 w-full hover:scale-110 transition duration-300" to={`/product/${orderItem.product.id}`}>
+            <img src={orderItem.product.image} alt={orderItem.product.name} className="w-full h-auto object-scale-down" />
+          </Link>
           <div className="">
-            <div className="product-name">{CartItem.product.name}</div>
-            <div className="product-description">{CartItem.product.category}</div>
-            <Rating value={CartItem.product.rating} readOnly cancel={false} />
-            <div className="product-price">{CartItem.price} ₺</div>
-            <div className="product-quantity">Quantity: {CartItem.quantity}</div>
-            <div className="product-quantity">Color: {CartItem.color}</div>
-            <div className="product-quantity">Size: {CartItem.size}</div>
+            <div className="product-name">{orderItem.product.name}</div>
+            <Rating value={orderItem.product.rating} readOnly cancel={false}
+              pt={{
+                onIcon: { className: '!text-primary' }
+              }} />
+            <div className="product-price">{orderItem.price} ₺</div>
+            <div className="product-quantity">Quantity: {orderItem.quantity}</div>
+            <div className="product-quantity">Color: {orderItem.color}</div>
+            <div className="product-quantity">Size: {orderItem.size}</div>
           </div>
         </div>
 
@@ -124,38 +104,44 @@ const UserOrders = () => {
       <Messages ref={msgs} />
 
       {/* order */}
+
       <div className="w-full flex flex-col gap-7">
-        <Fieldset className="w-full flex flex-col shadow-lg gap-9" toggleable
-          legend={legendTemplate()}
-          collapseIcon="pi pi-chevron-down text-white"
-          collapsed={false}
-          expandIcon="pi pi-chevron-up text-white"
-          pt={{
-            legend: { className: "w-full !bg-slate-600 " },
-            legendTitle: { className: 'w-full !flex !flex-col' },
+        {orders?.map((order) => {
+          return <Fieldset className="w-full flex flex-col shadow-lg gap-9" toggleable
+            key={"order-" + order.id}
+            legend={legendTemplate(order)}
+            collapseIcon="pi pi-chevron-down text-white"
+            collapsed={false}
+            expandIcon="pi pi-chevron-up text-white"
+            pt={{
+              legend: { className: "w-full !bg-slate-600 " },
+              legendTitle: { className: 'w-full !flex !flex-col' },
 
-          }}
-        >
-          <div className="flex flex-col gap-6 sm:max-w-none max-w-xs w-full">
+            }}
+          >
+            <div className="flex flex-col gap-6 sm:max-w-none max-w-xs w-full">
 
-            {/* order items */}
-            <div className="w-full flex flex-col">
-              <DataView value={exCartItems} itemTemplate={OrderItemTemplate} className="flex flex-col gap-4" />
+              {/* order items */}
+              <div className="w-full flex flex-col">
+                <DataView value={order.orderItems} itemTemplate={OrderItemTemplate} className="flex flex-col gap-4" />
+              </div>
+
+              {/* order address */}
+              {orderAddressTemplate(order.address)}
+
+              {/* order status */}
+              <div className="flex flex-col w-full gap-7 overflow-x-auto">
+
+                <h3 className="text-2xl text-primaryDark font-semibold">Sipariş Durumu</h3>
+                <Steps model={steps} readOnly activeIndex={
+                  orderStatus.findIndex((item) => item.value === order.status)
+                } className="w-full min-w-[600px]" />
+              </div>
             </div>
 
-            {/* order address */}
-            {orderAddressTemplate()}
 
-            {/* order status */}
-            <div className="flex flex-col w-full gap-7 overflow-x-auto">
-
-              <h3 className="text-2xl text-primaryDark font-semibold">Sipariş Durumu</h3>
-              <Steps model={steps} readOnly activeIndex={3} className="w-full min-w-[600px]"/>
-            </div>
-          </div>
-
-
-        </Fieldset>
+          </Fieldset>
+        })}
       </div>
     </motion.div>
   )
