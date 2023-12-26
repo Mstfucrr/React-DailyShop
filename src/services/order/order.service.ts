3b5fc7bee0a3e84e408921922031237b051a96b6
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-12-26 23:40:39 +0300

    add cancel order service function

--- src/services/order/order.service.ts
+++ src/services/order/order.service.ts
@@ -2,21 +2,24 @@ import { IOrderRequest, IaddToCartRequest } from "./types";
 import { makeRequest } from "../base/base";
 
 // Cart
-export const getCart = async (token: string) => 
+export const getCart = async (token: string) =>
     await makeRequest<any>("Carts", "GET", null, token);
 
-export const addToCart = async (productId: number,input: IaddToCartRequest, token: string) => 
+export const addToCart = async (productId: number, input: IaddToCartRequest, token: string) =>
     await makeRequest<any>(`Carts?productId=${productId}`, "POST", input, token);
 
 export const updateCart = async (id: number, input: any, token: string) =>
     await makeRequest<any>(`Carts/Update/${id}`, "PUT", input, token);
 
-export const removeFromCart = async (id: number, token: string) => 
+export const removeFromCart = async (id: number, token: string) =>
     await makeRequest<any>(`Carts/Delete/${id}`, "DELETE", null, token);
 
 // Order
-export const getOrders = async (token: string) => 
+export const getOrders = async (token: string) =>
     await makeRequest<any>("Orders", "GET", null, token);
 
 export const createOrder = async (input: IOrderRequest, token: string) =>
     await makeRequest<any>("Orders", "POST", input, token);
+
+export const cancelOrder = async (orderId: number, status: string, token: string) =>
+    await makeRequest<any>(`Orders/${orderId}`, "PUT", status, token);
