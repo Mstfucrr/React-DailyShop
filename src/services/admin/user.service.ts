b446a28b12e293df9d929c0226ceb51bf4895e03
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-12-31 17:31:26 +0300

    Update API request types in user.service.ts

--- src/services/admin/user.service.ts
+++ src/services/admin/user.service.ts
@@ -10,25 +10,25 @@ export const blockUser = async (id: number, token: string) =>
 
 // USER ADDRESS
 export const fetchAddressByUserId = async (id: number, token: string) =>
-    await makeRequest(`Profiles/GetListAddressByUserId?id=${id}`, "GET", null, token);
+    await makeRequest<any>(`Profiles/GetListAddressByUserId?id=${id}`, "GET", null, token);
 
 // USER REVIEWS (FETH AND UPDATE)
 export const fetchReviewsByUserId = async (id: number, token: string) =>
-    await makeRequest(`/Reviews/GetListByUserId?UserId=${id}`, "GET", token);
+    await makeRequest<any>(`/Reviews/GetListByUserId?UserId=${id}`, "GET", token);
 
 export const updateReviewStatus = async (id: number, status: string, token: string) =>
-    await makeRequest(`/admin/users/UpdateReviewStatus/${id}?status=${status}`, "PUT", null, token);
+    await makeRequest<any>(`/admin/users/UpdateReviewStatus/${id}?status=${status}`, "PUT", null, token);
 
 // USER PRODUCTS (FETH AND UPDATE)
 export const fetchPaddingProductByUserId = async (id: number, token: string) =>
-    await makeRequest(`/admin/Users/${id}/Products`, "GET", null, token);
+    await makeRequest<any>(`/admin/Users/${id}/Products`, "GET", null, token);
 
 export const updateProductApprovalStatus = async (id: number, IsApproved: boolean, token: string) =>
     await makeRequest<any>(`/Admin/Products/UpdateStatus/${id}?IsApproved=${IsApproved}`, "PUT", null, token);
 
 // USER ORDERS
 export const fetchOrdersByUserId = async (id: number, token: string) =>
-    await makeRequest(`/admin/Orders/${id}`, "GET", null, token);
+    await makeRequest<any>(`/admin/Orders/${id}`, "GET", null, token);
 
 export const updateOrderStatus = async (orderId: number, status: string, token: string) =>
     await makeRequest<any>(`Orders/${orderId}`, "PUT", status, token);
