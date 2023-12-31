1aa492c7e1bd744d70dbce97fda410842a0182d9
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-12-31 22:51:42 +0300

    Add reportUser function to report.service.ts

--- src/services/report/report.service.ts
+++ src/services/report/report.service.ts
@@ -2,3 +2,6 @@ import { makeRequest } from "../base/base"
 
 export const reportReview = async (reviewId: number,input: any, token: string) =>
     await makeRequest<any>(`/Reviews/ReportReview/${reviewId}`, "POST", input, token);
+
+export const reportUser = async (userId: number,input: any, token: string) =>
+    await makeRequest<any>(`/Users/ReportUser/${userId}`, "POST", input, token);
