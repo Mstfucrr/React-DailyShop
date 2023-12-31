5633e4d04899a8782f9bf8299056e813ca69f2c8
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2023-12-31 17:31:14 +0300

    Add deleteProduct function to products.service.ts

--- src/services/admin/products.service.ts
+++ src/services/admin/products.service.ts
@@ -1,6 +1,10 @@
 import { makeRequest } from "../base/base";
+import { deleteProduct } from "../product/product.service";
 
 
 // PRODUCTS
 export const getAllProducts = async (token: string) =>
     await makeRequest<any>(`Products`, "GET", null, token);
+
+// deleteProduct
+export { deleteProduct }
