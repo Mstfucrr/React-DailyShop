3b3a4b60e59f382587cf1018dc9752891a24b7da
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2024-01-17 19:18:41 +0300

    Update image upload and updat styles

--- src/components/Seller/ImageUpload.tsx
+++ src/components/Seller/ImageUpload.tsx
@@ -1,141 +1,169 @@
-import { Tooltip } from "primereact/tooltip"
-import { FileUpload, FileUploadHandlerEvent, FileUploadUploadEvent, ItemTemplateOptions, } from 'primereact/fileupload';
-import { useRef, useState } from "react";
+import { Tooltip } from "primereact/tooltip";
 import { Button } from "primereact/button";
-import { Tag } from 'primereact/tag';
-
+import { useDispatch } from "react-redux";
+import { SET_TOAST } from "@/store/Toast";
+import { IToast } from "@/store/Toast/type";
+import { FaTimes } from "react-icons/fa";
+import { useCallback } from "react";
 
 type Props = {
-    setcoverImage: React.Dispatch<React.SetStateAction<File | null>>
-    setImages: React.Dispatch<React.SetStateAction<File[]>>
-}
+  setcoverImage: React.Dispatch<React.SetStateAction<File | null>>;
+  setImages: React.Dispatch<React.SetStateAction<File[]>>;
+  productImages: File[] | null;
+  coverImage: File | null;
+};
 
-const ImageUpload = (
-    { setcoverImage, setImages }: Props) => {
-    const fileUploadRef = useRef(null);
-    const [totalSize, setTotalSize] = useState(0);
+const ImageUpload = ({
+  setcoverImage,
+  setImages,
+  productImages,
+  coverImage,
+}: Props) => {
+  const handleCoverImage = (e: any) => {
+    if (e.target.files == null) return;
+    setcoverImage(e.target.files[0]);
+  };
 
-    const emptyTemplate = () => {
-        return (
-            <div className="flex items-center flex-col">
-                <i className="pi pi-image mt-3 p-5" style={{ fontSize: '5em', borderRadius: '50%', backgroundColor: 'var(--surface-b)', color: 'var(--surface-d)' }}></i>
-                <span style={{ fontSize: '1.2em', color: 'var(--text-color-secondary)' }} className="my-5">
-                    Resmi Buraya Sürükleyip Bırakın
-                </span>
-            </div>
-        );
-    };
-    const onTemplateSelect = (e: FileUploadUploadEvent) => {
-        let _totalSize = totalSize;
-        let files = e.files;
-
-        for (let i = 0; i < files.length; i++) {
-            _totalSize += files[i].size || 0;
-        }
-        setTotalSize(_totalSize);
-    };
-
-    const onTemplateClear = () => {
-        setTotalSize(0);
-    };
-
-    const coverItemTemplate = (inFile: object, props: ItemTemplateOptions) => {
-        const file = inFile as any;
-        setcoverImage(file as File)
-        return (
-            <div className="flex items-center flex-wrap w-full">
-                <div className="flex items-center w-full gap-4">
-                    <div>
-                        <img alt={file.name} role="presentation" src={file.objectURL} />
-                    </div>
-                    <div>
-                        <Button type="button" icon="pi pi-times" className="p-button-outlined p-button-rounded p-button-danger ml-auto"
-                            onClick={() => { setcoverImage(null); onTemplateRemove(file, props.onRemove) }} />
-                    </div>
-
-                </div>
-            </div>
-        );
-    };
-
-    const itemTemplate = (inFile: object, props: ItemTemplateOptions) => {
-        const file = inFile as any;
-        return (
-            <div className="flex items-center flex-wrap justify-between gap-y-2">
-                <div className="flex items-center ">
-                    <img alt={file.name} role="presentation" src={file.objectURL} width={100} />
-                    <span className="flex flex-col text-left ml-3 ">
-                        {file.name.length > 15 ? file.name.slice(0, 15) + "..." : file.name}
-                        <small>{new Date().toLocaleDateString()}</small>
-                    </span>
-                </div>
-                <div className="flex gap-x-2 justify-between xl:w-2/5 md:w-full">
-
-                    <Tag value={props.formatSize} severity="warning" className="px-3 py-2" />
-                    <Button type="button" icon="pi pi-times" className="p-button-outlined p-button-rounded p-button-danger ml-auto"
-                        onClick={() => { setImages((prev) => { return prev.filter((item) => item !== file) }); onTemplateRemove(file, props.onRemove) }} />
-                </div>
-            </div>
-        );
-    };
-
-    const onTemplateRemove = (file: File, callback: Function) => {
-        setTotalSize(totalSize - file.size);
-        callback();
-    };
-
-    const customBase64Uploader = async (event: FileUploadHandlerEvent) => setImages(event.files)
-
-    const chooseOptions = { icon: 'pi pi-fw pi-images', className: 'custom-choose-btn p-button-rounded p-button-outlined' };
-    const cancelOptions = { icon: 'pi pi-fw pi-times', className: 'custom-cancel-btn p-button-danger p-button-rounded p-button-outlined' };
-    const ImagesUploadOptions = { icon: 'pi pi-fw pi-cloud-upload', iconOnly: false, className: 'custom-upload-btn p-button-success p-button-rounded p-button-outlined' };
+  const RenderCoverImage = useCallback(() => {
     return (
-        <>
-            <Tooltip target=".custom-choose-btn" content="Choose" position="bottom" />
-            {/* for cover phote */}
-            <div className="flex flex-col gap-y-10 w-full">
-                <h2 className="text-2xl font-semibold">Kapak Fotoğrafı</h2>
-                <FileUpload
-                    className="w-full"
-                    ref={fileUploadRef}
-                    emptyTemplate={emptyTemplate}
-                    onSelect={() => onTemplateSelect}
-                    maxFileSize={10000000}
-                    itemTemplate={coverItemTemplate}
-                    name="demo[]" url="/api/upload" accept="image/*" customUpload
-                    onClear={() => onTemplateClear}
-                    cancelOptions={{ className: "!hidden" }}
-                    uploadOptions={{ className: "!hidden" }}
-                    chooseOptions={chooseOptions}
-                    chooseLabel="Seç"
-                    multiple={false}
-                />
-
-                {/* for another photos ( multi ) */}
-                <h2 className="text-2xl font-semibold">Diğer Fotoğraflar</h2>
-                <FileUpload
-                    className="w-full"
-                    ref={fileUploadRef}
-                    onSelect={() => onTemplateSelect}
-                    maxFileSize={10000000}
-                    itemTemplate={itemTemplate}
-                    name="demo[]" accept="image/*" uploadHandler={customBase64Uploader}
-                    onClear={() => onTemplateClear}
-                    cancelOptions={cancelOptions}
-                    uploadOptions={ImagesUploadOptions}
-                    chooseOptions={chooseOptions}
-                    multiple
-                    contentClassName="w-full"
-                    customUpload
-                    uploadLabel="Yükle"
-                    chooseLabel="Seç"
-                    cancelLabel="Temizle"
-                />
-
+      <>
+        <div className="relative flex justify-center h-80">
+          {coverImage !== null ? (
+            <img
+              src={URL.createObjectURL(coverImage)}
+              alt={coverImage.name || "resim"}
+              className="w-[350px] h-auto object-contain"
+            />
+          ) : (
+            <div className="flex items-center justify-center w-[350px] h-[350px] bg-gray-200">
+              <i className="pi pi-image" style={{ fontSize: "5em" }}></i>
             </div>
+          )}
+          <div
+            className="absolute top-2/3 bg-white bg-opacity-60 rounded-full
+                              hover:bg-opacity-80 transition-all duration-300 ease-in-out transform hover:scale-110
+                              border border-green-500 border-dashed mt-28
+                          "
+          >
+            <Button
+              icon="pi pi-upload"
+              severity="success"
+              rounded
+              text={true}
+              className="w-full h-full"
+              onClick={() => {
+                const input = document.createElement("input");
+                input.type = "file";
+                input.accept = "image/*";
+                input.onchange = handleCoverImage;
+                input.click();
+              }}
+            >
+              <div className="px-5">
+                <span className="text-sm">Kapak Fotoğrafı Yükle</span>
+              </div>
+            </Button>
+          </div>
+        </div>
+      </>
+    );
+  }, [coverImage]);
 
-        </>
-    )
-}
+  const RenderAnotherImages = useCallback(() => {
+    return (
+      <>
+        {productImages != null && (
+          <div className="flex flex-row gap-4 w-full flex-wrap mt-3">
+            {productImages.map((image) => (
+              <div
+                className="flex relative justify-end md:w-[300px] w-full h-auto rounded-3xl border-dashed border border-blue-600"
+                key={image.name}
+              >
+                <FaTimes
+                  className="text-red-400 text-2xl cursor-pointer absolute float-right right-3 top-3"
+                  onClick={() => {
+                    handleRemoveImage(image);
+                  }}
+                />
+                <img
+                  src={URL.createObjectURL(image)}
+                  alt="resim"
+                  className=" object-contain p-2 "
+                />
+              </div>
+            ))}
 
-export default ImageUpload
+            <button
+              className="md:w-[300px] w-full text-2xl text-blue-600 h-auto border border-blue-600 border-dashed rounded-3xl
+                                                hover:bg-blue-600 hover:text-white transition-all duration-300 ease-in-out transform hover:scale-105 bg-opacity-10"
+              onClick={() => {
+                const input = document.createElement("input");
+                input.type = "file";
+                input.accept = "image/*";
+                input.multiple = true;
+                input.click();
+                input.onchange = (e) => {
+                  handleAddImage(e as any);
+                };
+              }}
+            >
+              +
+            </button>
+          </div>
+        )}
+      </>
+    );
+  }, [productImages]);
+
+  const dispatch = useDispatch();
+
+  const handleAddImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files == null) return;
+    if (e.target.files.length === 0) return;
+    // aynı dosya varsa hata ver
+    for (const file of e.target.files) {
+      if (productImages?.find((img) => img.name === file.name)) {
+        const toast: IToast = {
+          severity: "warn",
+          summary: "Uyarı",
+          detail: "Aynı dosyadan var",
+          life: 5000,
+        };
+        dispatch(SET_TOAST(toast));
+        return;
+      }
+    }
+    const files = Array.from(e.target.files);
+    setImages((prev) => [...prev, ...files]);
+  };
+
+  const handleRemoveImage = (image: File) => {
+    if (productImages === null) return;
+    if (typeof image === "string") {
+      setImages(productImages.filter((img) => img !== image));
+      return;
+    }
+    setImages(productImages.filter((img) => img !== image));
+  };
+
+  return (
+    <>
+      <Tooltip target=".custom-choose-btn" content="Choose" position="bottom" />
+      {/* for cover phote */}
+      <div className="flex flex-col gap-y-28 w-full">
+        <div className="flex flex-col gap-y-5 relative">
+          <h2 className="text-2xl font-semibold">Kapak Fotoğrafı</h2>
+          <RenderCoverImage />
+        </div>
+
+        {/* for another photos ( multi ) */}
+        <div className="flex flex-col gap-y-5 relative">
+          <h2 className="text-2xl font-semibold">Diğer Fotoğraflar</h2>
+          <RenderAnotherImages />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ImageUpload;
