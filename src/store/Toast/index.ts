cb06aecabef2930d0d8c9f6fbe236e0918ebdb32
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2024-01-17 18:12:11 +0300

    Refactor Toast store slice

--- src/store/Toast/index.ts
+++ src/store/Toast/index.ts
@@ -1,37 +1,35 @@
-import { createSlice } from '@reduxjs/toolkit';
-import React from 'react';
-import {
-    IToast
-} from './type';
+import { createSlice } from "@reduxjs/toolkit";
+import React from "react";
+import { IToast } from "./type";
+
+const initialState: IToast = {
+  severity: "success",
+  summary: "",
+  detail: "" as React.ReactNode,
+  life: 3000,
+};
 
 export const toastSlice = createSlice({
-    name: 'toast',
-    initialState: {
-        severity: 'success',
-        summary: '',
-        detail: '' as React.ReactNode,
-        life: 3000,
-    } as IToast,
-    reducers: {
-        SET_TOAST: (state, action) => {
-            state.severity = action.payload.severity;
-            state.summary = action.payload.summary;
-            state.detail = action.payload.detail;
-            state.life = action.payload.life;
-        },
-        CLEAR_TOAST: (state) => {
-            state.severity = 'success';
-            state.summary = '';
-            state.detail = '' as React.ReactNode;
-            state.life = 3000;
-        }
-
-    }
-
+  name: "toast",
+  initialState: initialState,
+  reducers: {
+    SET_TOAST: (state, action) => {
+      state.severity = action.payload.severity;
+      state.summary = action.payload.summary;
+      state.detail = action.payload.detail;
+      state.life = action.payload.life;
+    },
+    CLEAR_TOAST: (state) => {
+      state.severity = "success";
+      state.summary = "";
+      state.detail = "" as React.ReactNode;
+      state.life = 3000;
+    },
+  },
 });
 
 export const { SET_TOAST, CLEAR_TOAST } = toastSlice.actions;
 
-
 export default toastSlice.reducer;
 
+export const toastSelector = (state: { toast: IToast }) => state.toast;
