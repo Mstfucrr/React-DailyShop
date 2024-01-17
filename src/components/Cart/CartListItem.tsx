06ef93a1156cf53f0d295ba03e8024278dcd85a1
Mustafa Uçar <mustafa19071907mustafa@gmail.com>
2024-01-18 02:12:07 +0300

    Refactor Cart components

--- src/components/Cart/CartListItem.tsx
+++ src/components/Cart/CartListItem.tsx
@@ -1,126 +1,153 @@
-import { useEffect, useState } from 'react'
-import { FaMinus, FaPlus, FaTimes, FaTrash } from 'react-icons/fa'
-import { ICartItem } from '@/shared/types'
-import { removeFromCart, updateCart } from '@/services/order/order.service'
-import to from 'await-to-js'
-import { authSelector } from '@/store/auth'
-import { useDispatch, useSelector } from 'react-redux'
-import { SET_TOAST } from '@/store/Toast'
-import { IToast } from '@/store/Toast/type'
-import { Link } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { FaMinus, FaPlus, FaTimes, FaTrash } from "react-icons/fa";
+import { ICartItem } from "@/shared/types";
+import { removeFromCart, updateCart } from "@/services/order/order.service";
+import to from "await-to-js";
+import { authSelector } from "@/store/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { SET_TOAST } from "@/store/Toast";
+import { IToast } from "@/store/Toast/type";
+import { Link } from "react-router-dom";
 
+const CartListItem = ({
+  cartItem,
+  fetchCart,
+}: {
+  cartItem: ICartItem;
+  fetchCart: () => void;
+}) => {
+  const [quantity, setQuantity] = useState(cartItem.quantity);
+  const [total, setTotal] = useState(0);
 
-
-
-const CartListItem = (
-  { cartItem, fetchCart }: { cartItem: ICartItem, fetchCart: () => void }
-) => {
-
-  const [quantity, setQuantity] = useState(cartItem.quantity)
-  const [total, setTotal] = useState(0)
-
-  const { token } = useSelector(authSelector)
-  const dispatch = useDispatch()
+  const { token } = useSelector(authSelector);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    setTotal(cartItem.product.price * quantity)
-  }, [quantity, cartItem])
-
+    setTotal(cartItem.product.price * quantity);
+  }, [quantity, cartItem]);
 
   const handleUpdateItem = async (quantity: number) => {
-    setQuantity(quantity)
-    const [err, data] = await to(updateCart(cartItem.id, { "quantity": quantity }, token))
+    setQuantity(quantity);
+    const [err, data] = await to(
+      updateCart(cartItem.id, { quantity: quantity }, token)
+    );
     if (err) {
-      const toast: IToast = { severity: 'error', summary: "Hata", detail: err.message, life: 3000 }
-      dispatch(SET_TOAST(toast))
+      const toast: IToast = {
+        severity: "error",
+        summary: "Hata",
+        detail: err.message,
+        life: 3000,
+      };
+      dispatch(SET_TOAST(toast));
     }
-    const toast: IToast = { severity: 'success', summary: "Başarılı", detail: data.message, life: 3000 }
-    dispatch(SET_TOAST(toast))
-  }
+    const toast: IToast = {
+      severity: "success",
+      summary: "Başarılı",
+      detail: data.message,
+      life: 3000,
+    };
+    dispatch(SET_TOAST(toast));
+  };
 
   const handleRemoveItem = async () => {
-
-    const [err, data] = await to(removeFromCart(cartItem.id, token))
+    const [err, data] = await to(removeFromCart(cartItem.id, token));
     if (err) {
-      const toast: IToast = { severity: 'error', summary: "Hata", detail: err.message, life: 3000 }
-      dispatch(SET_TOAST(toast))
+      const toast: IToast = {
+        severity: "error",
+        summary: "Hata",
+        detail: err.message,
+        life: 3000,
+      };
+      dispatch(SET_TOAST(toast));
     }
-    const toast: IToast = { severity: 'success', summary: "Başarılı", detail: data.message, life: 3000 }
-    dispatch(SET_TOAST(toast))
-  }
+    const toast: IToast = {
+      severity: "success",
+      summary: "Başarılı",
+      detail: data.message,
+      life: 3000,
+    };
+    dispatch(SET_TOAST(toast));
+  };
 
   return (
-    <tr className='bg-white'>
-      <td className='p-3 border border-solid border-secondary text-left pl-10 '>
+    <tr className="bg-white">
+      <td className="p-3 border border-solid border-secondary text-left pl-10 ">
         <div className="flex flex-row flex-wrap gap-6 items-center justify-between h-full">
-          <Link to={`/product/${cartItem?.product?.id}`} className='text-primary hover:text-red-800 hover:scale-105 transition-all duration-300 ease-in-out'>
-            <img src={cartItem?.product?.image as string} width={50} alt=""
-              className='inline-block object-cover w-16 h-auto rounded-md'
+          <Link
+            to={`/product/${cartItem?.product?.id}`}
+            className="text-primary hover:text-red-800 hover:scale-105 transition-all duration-300 ease-in-out"
+          >
+            <img
+              src={cartItem?.product?.image as string}
+              width={50}
+              alt=""
+              className="inline-block object-cover w-16 h-auto rounded-md"
             />
-            <span className='ml-3'>{cartItem?.product?.name}</span>
+            <span className="ml-3">{cartItem?.product?.name}</span>
           </Link>
           <div className="">
-
-            <p className='block text-sm mt-2'>Renk: {cartItem?.color}</p>
-            <p className='block text-sm mt-2'>Beden: {cartItem?.size}</p>
+            <p className="block text-sm mt-2">Renk: {cartItem?.color}</p>
+            <p className="block text-sm mt-2">Beden: {cartItem?.size}</p>
           </div>
         </div>
       </td>
-      <td className='p-3 border border-solid border-secondary align-middle'>
-        {cartItem?.product?.price}
+      <td className="p-3 border border-solid border-secondary align-middle">
+        {cartItem?.product?.price} ₺
       </td>
-      <td className='p-3 border border-solid border-secondary align-middle'>
+      <td className="p-3 border border-solid border-secondary align-middle">
         <div className="relative flex flex-nowrap justify-center items-stretch mx-auto max-w-[100px]">
-
           <div>
-            <button className='inline-block bg-primary text-[#212529] border-primary py-2 px-2 leading-6 
+            <button
+              className="inline-block bg-primary text-[#212529] border-primary py-2 px-2 leading-6 
                   hover:text-white hover:bg-primaryDark transition-all
-                  duration-300 ease-in-out'
+                  duration-300 ease-in-out"
               onClick={() => {
                 if (quantity > 1)
-                  handleUpdateItem(quantity - 1).then(fetchCart)
-                else
-                  handleRemoveItem().then(fetchCart)
+                  handleUpdateItem(quantity - 1).then(fetchCart);
+                else handleRemoveItem().then(fetchCart);
               }}
             >
               {quantity === 1 ? <FaTrash /> : <FaMinus />}
             </button>
           </div>
 
-          <p className='relative w-[1%] flex-[1_1_auto] text-center bg-secondary py-1 px-2
-                outline-none text-sm'
+          <p
+            className="relative w-[1%] flex-[1_1_auto] text-center bg-secondary py-1 px-2
+                outline-none text-sm"
           >
             {quantity}
           </p>
           <div>
-            <button className='inline-block bg-primary text-[#212529] border-primary py-2 px-2 leading-6 
+            <button
+              className="inline-block bg-primary text-[#212529] border-primary py-2 px-2 leading-6 
                   hover:text-white hover:bg-primaryDark transition-all
-                  duration-300 ease-in-out'
+                  duration-300 ease-in-out"
               onClick={() => {
-                handleUpdateItem(quantity + 1).then(fetchCart)
+                handleUpdateItem(quantity + 1).then(fetchCart);
               }}
             >
-              <FaPlus className='' />
+              <FaPlus className="" />
             </button>
           </div>
-
         </div>
       </td>
-      <td className='p-3 border border-solid border-secondary align-middle'>
-        ${total}
+      <td className="p-3 border border-solid border-secondary align-middle">
+        {total} ₺
       </td>
-      <td className='p-3 border border-solid border-secondary align-middle'>
-        <button className='inline-block bg-primary text-[#212529] border-primary py-2 px-2 leading-6
+      <td className="p-3 border border-solid border-secondary align-middle">
+        <button
+          className="inline-block bg-primary text-[#212529] border-primary py-2 px-2 leading-6
               hover:text-white hover:bg-primaryDark transition-all
-              duration-300 ease-in-out'
-          onClick={() => { handleRemoveItem().then(fetchCart) }}
+              duration-300 ease-in-out"
+          onClick={() => {
+            handleRemoveItem().then(fetchCart);
+          }}
         >
-          <FaTimes className='' />
+          <FaTimes className="" />
         </button>
-
       </td>
     </tr>
-  )
-}
+  );
+};
 
-export default CartListItem
+export default CartListItem;
--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,136 +1,137 @@
-import { useEffect, useRef, useState } from 'react'
-import CartListItem from './CartListItem'
-import { ICartItem } from '@/shared/types'
-import { Link } from 'react-router-dom'
-import { getCart } from '@/services/order/order.service'
-import { Messages } from 'primereact/messages'
-import to from 'await-to-js'
-import { useSelector } from 'react-redux'
-import { authSelector } from '@/store/auth'
+import { useEffect, useRef, useState } from "react";
+import CartListItem from "./CartListItem";
+import { ICartItem } from "@/shared/types";
+import { Link } from "react-router-dom";
+import { getCart } from "@/services/order/order.service";
+import { Messages } from "primereact/messages";
+import to from "await-to-js";
+import { useSelector } from "react-redux";
+import { authSelector } from "@/store/auth";
 
 const Cart = () => {
+  const [cartItems, setCartItems] = useState<[] | ICartItem[]>([]);
+  const [cartTotal, setCartTotal] = useState(0);
+  const msgs = useRef<Messages>(null);
 
-    const [cartItems, setCartItems] = useState<[] | ICartItem[]>([])
-    const [cartTotal, setCartTotal] = useState(0)
-    const msgs = useRef<Messages>(null)
-
-    const { token } = useSelector(authSelector)
-    const fetchCart = async () => {
-        const [err, data] = await to(getCart(token))
-        if (err) {
-            msgs.current?.clear()
-            msgs.current?.show([
-                { sticky: true, severity: 'error', summary: 'Sistematik Hata', detail: err.message }
-            ]);
-            return
-        }
-        setCartItems(data.data)
-        console.log(data.data)
-        if (data.data && data.data.length > 0) {
-            let total = 0
-            data.data.map((item: ICartItem) => {
-                total += item.product.price * item.quantity
-            })
-            setCartTotal(total)
-        }
-        else {
-            setCartTotal(0)
-            msgs.current?.clear()
-            msgs.current?.show([
-                { sticky: true, severity: 'info', summary: 'Sepetiniz Boş', detail: 'Sepetinizde ürün bulunmamaktadır.', closable: false }
-            ]);
-        }
+  const { token } = useSelector(authSelector);
+  const fetchCart = async () => {
+    const [err, data] = await to(getCart(token));
+    if (err) {
+      msgs.current?.clear();
+      msgs.current?.show([
+        {
+          sticky: true,
+          severity: "error",
+          summary: "Sistematik Hata",
+          detail: err.message,
+        },
+      ]);
+      return;
     }
+    setCartItems(data.data);
+    console.log(data.data);
+    if (data.data && data.data.length > 0) {
+      let total = 0;
+      data.data.map((item: ICartItem) => {
+        total += item.product.price * item.quantity;
+      });
+      setCartTotal(total);
+    } else {
+      setCartTotal(0);
+      msgs.current?.clear();
+      msgs.current?.show([
+        {
+          sticky: true,
+          severity: "info",
+          summary: "Sepetiniz Boş",
+          detail: "Sepetinizde ürün bulunmamaktadır.",
+          closable: false,
+        },
+      ]);
+    }
+  };
 
-    useEffect(() => { fetchCart() }, [])
+  useEffect(() => {
+    fetchCart();
+  }, []);
 
-
-    return (
-        <div className="flex md:flex-row flex-wrap flex-col w-full gap-y-9 lg:px-16 my-24 px-[15px]">
-            {cartItems && cartItems.length > 0
-                && <>
-                    <div className="w-2/3 md:px-9">
-                        <table className="w-full text-[#6F6F6F] mb-0 text-center border-0 border-collapse">
-                            <thead className="bg-secondary text-black">
-                                <tr>
-                                    <th className='p-3 border border-solid border-secondary'>Ürünler</th>
-                                    <th className='p-3 border border-solid border-secondary'>Fiyat</th>
-                                    <th className='p-3 border border-solid border-secondary'>Miktar</th>
-                                    <th className='p-3 border border-solid border-secondary'>Toplam</th>
-                                    <th className='p-3 border border-solid border-secondary'>Kaldır</th>
-                                </tr>
-                            </thead>
-                            <tbody className='align-middle'>
-
-                                {cartItems.map((cartItem) => (
-                                    <CartListItem key={cartItem.id} cartItem={cartItem} fetchCart={fetchCart} />
-                                ))
-                                }
-
-                            </tbody>
-
-                        </table>
-
-                    </div>
-                    <div className="w-1/3">
-                        <form action="" className="mb-12">
-                            <div className="relative flex flex-wrap w-full items-stretch">
-                                <input className="relative flex-auto p-6 border border-solid outline-none border-secondary
-                                    text-[#212529] placeholder-[#212529] placeholder-opacity-50
-                                    h-[50px] "
-                                    type="text" placeholder="Coupon code" />
-                                <div className="-ml-[1px] flex">
-                                    <button className="bg-primary border border-solid border-transparent text-[#212529] py-[0.375rem] px-3">
-                                        Apply Coupon
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                        {/* card */}
-                        <div className="mb-12 border border-solid border-secondary
-                        flex flex-col relative h-min">
-                            {/* card header */}
-                            <div className="py-3 px-5 bg-secondary">
-                                <h4 className="font-semibold text-2xl text-black">
-                                    Sepet Özeti
-                                </h4>
-                            </div>
-                            {/* card body */}
-                            <div className="flex-auto p-5">
-                                <div className="flex justify-between mb-4 pt-1">
-                                    <h6 className="font-medium text-black">
-                                        Ara Toplam
-                                    </h6>
-                                    <h6 className="font-medium text-black">
-                                        ${cartTotal}
-                                    </h6>
-                                </div>
-                            </div>
-                            {/* card footer */}
-                            <div className="border border-solid border-secondary py-3 px-5">
-                                <div className="flex justify-between mt-2 text-black">
-                                    <h5 className="font-bold text-xl">
-                                        Toplam
-                                    </h5>
-                                    <h5 className="font-bold text-xl">
-                                        ${cartTotal}
-                                    </h5>
-                                </div>
-                                <Link className="bg-primary border border-solid border-transparent text-[#212529] py-4 px-3 mt-4 w-full
+  return (
+    <div className="flex md:flex-row flex-wrap flex-col w-full gap-y-9 lg:px-16 my-24 px-[15px]">
+      {cartItems && cartItems.length > 0 && (
+        <>
+          <div className="w-2/3 md:px-9">
+            <table className="w-full text-[#6F6F6F] mb-0 text-center border-0 border-collapse">
+              <thead className="bg-secondary text-black">
+                <tr>
+                  <th className="p-3 border border-solid border-secondary">
+                    Ürünler
+                  </th>
+                  <th className="p-3 border border-solid border-secondary">
+                    Fiyat
+                  </th>
+                  <th className="p-3 border border-solid border-secondary">
+                    Miktar
+                  </th>
+                  <th className="p-3 border border-solid border-secondary">
+                    Toplam
+                  </th>
+                  <th className="p-3 border border-solid border-secondary">
+                    Kaldır
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="align-middle">
+                {cartItems.map((cartItem) => (
+                  <CartListItem
+                    key={cartItem.id}
+                    cartItem={cartItem}
+                    fetchCart={fetchCart}
+                  />
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="w-1/3">
+            {/* card */}
+            <div
+              className="mb-12 border border-solid border-secondary
+                        flex flex-col relative h-min"
+            >
+              {/* card header */}
+              <div className="py-3 px-5 bg-secondary">
+                <h4 className="font-semibold text-2xl text-black">
+                  Sepet Özeti
+                </h4>
+              </div>
+              {/* card body */}
+              <div className="flex-auto p-5">
+                <div className="flex justify-between mb-4 pt-1">
+                  <h6 className="font-medium text-black">Ara Toplam</h6>
+                  <h6 className="font-medium text-black">{cartTotal} ₺</h6>
+                </div>
+              </div>
+              {/* card footer */}
+              <div className="border border-solid border-secondary py-3 px-5">
+                <div className="flex justify-between mt-2 text-black">
+                  <h5 className="font-bold text-xl">Toplam</h5>
+                  <h5 className="font-bold text-xl">{cartTotal} ₺</h5>
+                </div>
+                <Link
+                  className="bg-primary border border-solid border-transparent text-[#212529] py-4 px-3 mt-4 w-full
                                 hover:bg-primaryDark hover:border-primaryDark hover:text-white
                                 transition duration-300 ease-in-out flex justify-center"
-                                    to="/checkout"
-                                >
-                                    Sepeti Onayla
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                </>
-            }
-            {msgs && <Messages ref={msgs} className="w-1/2 ml-24" />}
-        </div >
-    )
-}
+                  to="/checkout"
+                >
+                  Sepeti Onayla
+                </Link>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+      {msgs && <Messages ref={msgs} className="w-1/2 ml-24" />}
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
